bd6c5abd7bcefa70b83bb47892d9eee0d34a48a8
Rafael Lima <rafaelsplima@gmail.com>
2024-05-21 20:54:32 -0300

    chore: componente pensamento

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamentos',
   templateUrl: './listar-pensamentos.component.html',
-  styleUrls: ['./listar-pensamentos.component.css']
+  styleUrls: ['./listar-pensamentos.component.css'],
 })
 export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'I love Desenvolvimento',
+      autoria: 'Rafael',
+      modelo: 'modelo3',
+    },
+    {
+      conteudo: 'I love Desenvolvimento',
+      autoria: 'David',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo:
+        'I love DesenvolvimentoI love DesenI love DesenvolvimentoI love DesenvolvimentoI love DesenvolvimentoI love DesenvolvimentoI love DesenvolvimentoI love DesenvolvimentoI love DesenvolvimentovolvimentoI love DesenvolvimentoI love DesenvolvimentoI I love DesenvolvimentoI love DesenvolvimentoI love DesenvolvimentoI love Desenvolvimentolove DesenvolvimentoI love Desenvolvimento',
+      autoria: 'David',
+      modelo: 'modelo2',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
